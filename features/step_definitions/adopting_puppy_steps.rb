4eab3bc67079e8acd2a43afb9f23c359f5b50dca
minhajahamed <idragonwarrior@gmail.com>
2014-09-02 14:25:23 +0600

    s

--- features/step_definitions/adopting_puppy_steps.rb
+++ features/step_definitions/adopting_puppy_steps.rb
@@ -3,7 +3,7 @@
   end
 
  When /^I click the View Details button for "([^"]*)"$/ do |name|
-    on(HomePage).select_puppy name
+    on(HomePage).select_puppy "asdasdsd"
  end
 
  When /^I click the Adopt Me button$/ do
